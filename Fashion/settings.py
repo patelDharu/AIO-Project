7390447c21fda427c853c2528f7d354f21b80df4
Dharu <168706043+patelDharu@users.noreply.github.com>
2025-04-21 18:11:48 +0530

    Update settings.py

--- Fashion/settings.py
+++ Fashion/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e#!ov(wfcd6(@bo+!2#%tn$(jbbh@3bz#wh2hx=z0xgi@hb@61
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['198.211.99.20','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['aio-project-3.onrender.com']
 
 
 # Application definition
